85f013d8f8665a9e60d85f021f740c59300bc7ce
AkashG15 <7875987176ag@gmail.com>
2023-05-18 11:31:33 +0530

    My First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+// React.createElement("h1",null,"This is my Heading")
+// document.getElementById("root")
+
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h4>My List</h4>
+      <ol>
+        <li>MI</li>
+        <li>TITAN</li>
+        <li>APPLE</li>
+      </ol>
+    </div>
+  );
+} 
+
+export default App;
+ 
